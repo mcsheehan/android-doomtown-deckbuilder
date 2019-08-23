18db515b5fd6981cf12097821eeb67156bc1e903
Mark Sheehan <mark.sheehan.uk@gmail.com>
2019-08-24 00:13:04 +0100

    Implementation of the choose identity card fragment
    implemented the choose identity fragment.
    It is now possible to select the card which has currently been snapped to by the layout manager.
    updated library versions

--- app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChooseIdentityFragment.kt
+++ app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChooseIdentityFragment.kt
@@ -4,42 +4,53 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.choose_identity.*
 import net.marksheehan.doomtowndeckbuilder.datamodel.CardModel
-import net.marksheehan.doomtowndeckbuilder.internetdatabase.DoomtownDbAccess
 import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.SnapHelper
+import net.marksheehan.doomtowndeckbuilder.internetdatabase.ParseCardListFromJsonFileUsingGson
 
 class ChooseIdentityFragment : Fragment()
 {
-    internal lateinit var cardAdapter: CardAdapter
-
-    internal var cardResult: DoomtownDbAccess.CardQueryCallback = object : DoomtownDbAccess.CardQueryCallback {
-
-        override fun success(cardListResult: List<CardModel>?) {
-
-            val clubs = cardListResult?.filter { it.suit == "Clubs" }
-            val outfitCardList = cardListResult?.filter { it.type == "Outfit" }
-
-            cardAdapter = CardAdapter(outfitCardList!!)
-
-            identityChooser.adapter = cardAdapter
-            cardAdapter.notifyDataSetChanged()
-        }
-
-        override fun failure() {
-        }
-    }
+    lateinit var snapHelper : LinearSnapHelper
+    lateinit var outfitCardList : List<CardModel>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.choose_identity, container, false);
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val snapHelper = LinearSnapHelper()
-        snapHelper.attachToRecyclerView(identityChooser)
-        val doomtownDbAccess = DoomtownDbAccess()
-        doomtownDbAccess.sendServerRequestCardList(cardResult)
+    fun SnapHelper.getSnapPosition(recyclerView: RecyclerView): Int {
+        val layoutManager = recyclerView.layoutManager ?: return RecyclerView.NO_POSITION
+        val snapView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
+        return layoutManager.getPosition(snapView)
     }
+
+    val onSelectButtonPressed = View.OnClickListener { view: View ->
+
+
+        val layoutManager = identityChooser.layoutManager!!
+        val snapView = snapHelper.findSnapView(layoutManager)!!
+        val snapPosition = layoutManager.getPosition(snapView)
+        val selectedCard = outfitCardList[snapPosition]
+        val title = selectedCard.title
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val cardModel : List<CardModel> = ParseCardListFromJsonFileUsingGson.parseCardListFromAssetFile(context!!,"card_list.json")
+
+        outfitCardList = cardModel.filter { it.type == "Outfit" }
+
+        identityChooser.adapter = FullScreenCardAdapter(outfitCardList)
+
+        snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(identityChooser)
+
+        selectButton.setOnClickListener(onSelectButtonPressed)
+
+
+    }
+
 }
