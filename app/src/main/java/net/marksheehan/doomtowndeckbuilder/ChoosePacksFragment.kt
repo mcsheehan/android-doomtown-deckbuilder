14633e025ee65ddcb95307677a4aa272be110360
Mark Sheehan <mark.sheehan.uk@gmail.com>
2019-09-01 11:36:17 +0100

    added the download images tool and persistance for choosing packs

--- app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChoosePacksFragment.kt
+++ app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChoosePacksFragment.kt
@@ -1,14 +1,19 @@
 package net.marksheehan.doomtowndeckbuilder
 
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.choose_pack_layout.*
 import kotlinx.android.synthetic.main.text_and_checkbox_item.view.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import net.marksheehan.doomtowndeckbuilder.adapters.TextAndBoolean
 import net.marksheehan.doomtowndeckbuilder.adapters.PackListAdapter
 import net.marksheehan.doomtowndeckbuilder.viewmodels.DoomtownCardsViewModel
@@ -17,12 +22,27 @@ class ChoosePacksFragment : Fragment(R.layout.choose_pack_layout) {
 
     lateinit var doomtownCardsViewModel : DoomtownCardsViewModel
 
+    fun serialiseSelectedPackMapToSharedPreferences(activity : Activity){
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        val map = doomtownCardsViewModel.selectedCardPacks.value
+        val jsonMapString = Gson().toJson(map)
+
+        editor.putString("selectedPacks", jsonMapString)
+        editor.apply()
+    }
+
     val checkedTextViewOnClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
         val item: TextAndBoolean = parent.getItemAtPosition(position) as TextAndBoolean
         item.checked = !item.checked
         view.checkedTextView.isChecked = item.checked
 
         this.doomtownCardsViewModel.selectedCardPacks.value!![item.name] = item.checked
+
+        GlobalScope.launch{
+            serialiseSelectedPackMapToSharedPreferences(activity!!)
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/net/marksheehan/doomtowndeckbuilder/viewmodels/DoomtownCardsViewModel.kt
+++ app/src/main/java/net/marksheehan/doomtowndeckbuilder/viewmodels/DoomtownCardsViewModel.kt
@@ -1,9 +1,12 @@
 package net.marksheehan.doomtowndeckbuilder.viewmodels
 
+import android.app.Activity
 import android.content.Context
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import net.marksheehan.doomtowndeckbuilder.datamodel.CardModel
 import net.marksheehan.doomtowndeckbuilder.internetdatabase.ParseCardListFromJsonFileUsingGson
 
@@ -31,7 +34,26 @@ class DoomtownCardsViewModel(context : Context) : ViewModel(){
         return allPacksSet
     }
 
-    private fun loadInitialSelectedCardPacks() : MutableLiveData<MutableMap<String, Boolean>> {
+    private fun deserialiseSelectedPackMapFromSharedPreferences(activity : Activity): Map<String, Boolean>? {
+
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE)
+
+        val defaultValue = ""
+        val jsonString = sharedPref.getString("selectedPacks", defaultValue)
+        if(jsonString == defaultValue)
+        {
+            return null
+        }
+
+        val cardModelType = object : TypeToken<Map<String, Boolean>>() {}.type
+        val cardListResult = Gson().fromJson<Map<String,Boolean>>(jsonString, cardModelType)
+        return cardListResult
+    }
+
+    private fun loadInitialSelectedCardPacks(context: Context) : MutableLiveData<MutableMap<String, Boolean>> {
+        //Load first. If this fails then initialise
+        deserialiseSelectedPackMapFromSharedPreferences()
+
         val cardPacksChecked =  MutableLiveData<MutableMap<String, Boolean>>()
         val cardPacksSelected : MutableMap<String, Boolean> = mutableMapOf()
 
