448dfa60b32a63e11e673c94378219c4eee240c1
Mark Sheehan <mark.sheehan.uk@gmail.com>
2019-10-03 13:21:44 +0100

    Added handling of all packs being deselected
    This caused no cards to be displayed in the ChooseIdentityFragmentView and this is now handled.

--- app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChooseIdentityFragment.kt
+++ app/src/main/java/net/marksheehan/doomtowndeckbuilder/ChooseIdentityFragment.kt
@@ -2,11 +2,13 @@ package net.marksheehan.doomtowndeckbuilder
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
 import kotlinx.android.synthetic.main.choose_identity.*
@@ -22,7 +24,7 @@ class ChooseIdentityFragment : Fragment(R.layout.choose_identity)
         InjectorUtils.provideCardViewerViewModel(requireContext())
     }
 
-    lateinit var snapHelper : LinearSnapHelper
+    lateinit var snapHelper : PagerSnapHelper
     lateinit var outfitCardList : List<CardModel>
 
     fun SnapHelper.getSnapPosition(recyclerView: RecyclerView): Int {
@@ -33,22 +35,29 @@ class ChooseIdentityFragment : Fragment(R.layout.choose_identity)
 
     val onSelectButtonPressed = View.OnClickListener { view: View ->
         val layoutManager = identityChooser.layoutManager!!
-        val snapView = snapHelper.findSnapView(layoutManager)!!
-        val snapPosition = layoutManager.getPosition(snapView)
-        val selectedCard = outfitCardList[snapPosition]
-        val title = selectedCard.title
 
-        Navigation.findNavController(view).navigate(R.id.action_chooseIdentity_to_chooseCards)
+        val snapView = snapHelper.findSnapView(layoutManager)
+
+        if(snapView != null) {
+            val snapPosition = layoutManager.getPosition(snapView)
+            val selectedCard = outfitCardList[snapPosition]
+            val title = selectedCard.title
+            Navigation.findNavController(view).navigate(R.id.action_chooseIdentity_to_chooseCards)
+        }
+        else {
+            Toast.makeText(context,"Select an identity. You may need to choose some packs.",Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
+        snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(identityChooser)
+        selectButton.setOnClickListener(onSelectButtonPressed)
+
         viewModel.allCardsFromSelectedPacks.observe(this, Observer<List<CardModel>> { cardList->
             outfitCardList = cardList.filter { it.type == "Outfit"}.sortedByDescending { it.gang }
             identityChooser.adapter = FullScreenCardAdapter(outfitCardList)
-            snapHelper = LinearSnapHelper()
-            snapHelper.attachToRecyclerView(identityChooser)
-            selectButton.setOnClickListener(onSelectButtonPressed)
         })
     }
 }
